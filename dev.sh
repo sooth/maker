b5fcde526fa00c9bab66fd588a2fbf2d2594b8c7
Cranky Kernel <thecrankykernel@gmail.com>
2018-12-09 09:54:18 -0600

    dev.sh: ignore packr generated files

--- dev.sh
+++ dev.sh
@@ -7,6 +7,6 @@ trap 'echo "Killing background jobs..."; kill $(jobs -p)' EXIT
 (cd webapp && npm start) &
 
 while true; do
-    find */* -name \*.go | \
+    find go -name \*.go | grep -v packr | \
 	entr -d -r sh -c "(cd go && make) && ./go/maker server"
 done
